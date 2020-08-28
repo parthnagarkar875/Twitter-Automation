63b0d95ebd8995b7f43f3e9c13cb0df6b393be1d
VirtualGoat <nagarkarparth875@gmail.com>
2020-08-29 03:05:53 +0530

    Done

---
+++ app.py
@@ -0,0 +1,37 @@
+import tweepy
+from flask import Flask
+import gspread
+from os import environ
+import sys
+from oauth2client.service_account import ServiceAccountCredentials
+from time import sleep
+from apscheduler.schedulers.background import BackgroundScheduler
+
+#Twitter API keys will be accessed from the environment variables. 
+consumer_key = environ['API_KEY']
+consumer_secret_key = environ['API_SECRET_KEY']
+access_token = environ['ACCESS_TOKEN']
+access_token_secret = environ['ACCESS_TOKEN_SECRET']
+
+#Initializing Google sheets API
+scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+creds=ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client=gspread.authorize(creds)
+sheet=client.open('Final tweets').sheet1
+
+def tweet_quote():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth,wait_on_rate_limit=True)
+
+    tweet = sheet.row_values(1)[0]          #Fetching the first row from Google sheet
+    api.update_status(status =tweet)        
+    sheet.delete_row(1)                       
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(tweet_quote,'interval', hours=3)
+sched.start()
+
+app = Flask(__name__)
+if __name__ == "__main__":    
+    app.run(host='0.0.0.0',port=environ.get('PORT'))
---
+++ readme.md
@@ -0,0 +1,13 @@
+
+
+https://dev.to/emcain/how-to-set-up-a-twitter-bot-with-python-and-heroku-1n39
+
+https://medium.com/datadriveninvestor/making-a-quote-tweeting-twitter-bot-with-python-tweepy-and-heroku-69a11cd3f47e
+
+https://medium.com/better-programming/introduction-to-apscheduler-86337f3bb4a6
+
+The last link has finally been followed by me. It uses APScheduler.
+
+At the end, I enabled the WORKER dyno.
+
+Also, I defined the Twitter API keys in environment variables of Heroku.
