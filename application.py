64355cd3472d8c72723f301a2de17f7d7c65fa82
VirtualGoat <nagarkarparth875@gmail.com>
2020-09-03 00:11:53 +0530

    Done and deployed

--- application.py
+++ application.py
@@ -3,13 +3,17 @@ from flask import Flask
 import gspread
 from os import environ
 import os
-import credentials
 import sys
 from oauth2client.service_account import ServiceAccountCredentials
 from time import sleep
 from apscheduler.schedulers.background import BackgroundScheduler
 
-#grabs the path for authentication keys for google sheets API from the client_secret.json file.  
+#Twitter API keys will be accessed from the environment variables. 
+consumer_key = 'zJVhlGfigW1xeDHK3y16lCnQZ'
+consumer_secret_key = 'NJPC86WrRlI6nojBzYHr1ZjxWQBTPiZ8lXYJeZhbESuwSIsWSE'
+access_token = '1277604089535160320-nzQwbJncqXQEaWzq23PUR4gN5N6kc1'
+access_token_secret = 'Ez48seBANENmIk5h8mqHAPGcaOL1YBJcZYIWeM9xMOruc'
+
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 my_file = os.path.join(THIS_FOLDER, 'client_secret.json')
 
@@ -20,26 +24,24 @@ client=gspread.authorize(creds)
 sheet=client.open('Final tweets').sheet1
 
 def tweet_quote():
-    #This function fetches the top tweet from the google sheet, posts it on twitter and then deletes it from the sheet. 
-    auth = tweepy.OAuthHandler(credentials.consumer_key, credentials.consumer_secret_key)
-    auth.set_access_token(credentials.access_token, credentials.access_token_secret)
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
+    auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth,wait_on_rate_limit=True)
 
     tweet = sheet.row_values(1)[0]          #Fetching the first row from Google sheet
     api.update_status(status =tweet)        
     sheet.delete_row(1)                       
 
-#Initializing the scheduler. 
 sched = BackgroundScheduler(daemon=True)
 sched.add_job(tweet_quote,'interval', hours=3)
 sched.start()
 
-app = Flask(__name__)
+application = app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Doing..."
 
 if __name__ == "__main__":    
-    app.run(host='0.0.0.0',port=8080)
+    app.run(debug=True)
 
