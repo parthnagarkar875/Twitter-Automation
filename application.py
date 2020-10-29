30c35faa60d33efc86fc87d0bf669da6fa2d6dde
VirtualGoat <nagarkarparth875@gmail.com>
2020-10-30 02:29:43 +0530

    Added 5 hour interval

--- application.py
+++ application.py
@@ -2,17 +2,12 @@ import tweepy
 from flask import Flask
 import gspread
 from os import environ
+import credentials
 import os
 import sys
 from oauth2client.service_account import ServiceAccountCredentials
 from time import sleep
 from apscheduler.schedulers.background import BackgroundScheduler
-
-#Twitter API keys will be accessed from the environment variables. 
-consumer_key = 'zJVhlGfigW1xeDHK3y16lCnQZ'
-consumer_secret_key = 'NJPC86WrRlI6nojBzYHr1ZjxWQBTPiZ8lXYJeZhbESuwSIsWSE'
-access_token = '1277604089535160320-nzQwbJncqXQEaWzq23PUR4gN5N6kc1'
-access_token_secret = 'Ez48seBANENmIk5h8mqHAPGcaOL1YBJcZYIWeM9xMOruc'
 
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 my_file = os.path.join(THIS_FOLDER, 'client_secret.json')
@@ -24,8 +19,8 @@ client=gspread.authorize(creds)
 sheet=client.open('Final tweets').sheet1
 
 def tweet_quote():
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
-    auth.set_access_token(access_token, access_token_secret)
+    auth = tweepy.OAuthHandler(credentials.consumer_key, credentials.consumer_secret_key)
+    auth.set_access_token(credentials.access_token, credentials.access_token_secret)    
     api = tweepy.API(auth,wait_on_rate_limit=True)
 
     tweet = sheet.row_values(1)[0]          #Fetching the first row from Google sheet
